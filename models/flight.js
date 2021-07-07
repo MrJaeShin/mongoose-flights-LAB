3c820273267a664aeed02d85a8b402a0b4fadb50
Judy Kim <judykim@Judys-MacBook-Pro.local>
2021-07-07 16:25:20 -0700

    added destination schema in flights model

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
-// optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
+
+const destinationSchema = new Schema({
+    airport: {
+        type: String,
+		enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+	},
+    arrival: {
+        type: Date,
+    },
+})
 
 const flightSchema = new Schema({
 	airline: { 
@@ -26,5 +35,4 @@ const flightSchema = new Schema({
     },
 });
 
-// Compile the schema into a model and export it
 module.exports = mongoose.model('Flight', flightSchema);
